33fbb5e67efd8b9b7fcdde1f952e2a8c45691a2c
TwuanMinn <twuanminn47@gmail.com>
2026-01-29 00:39:00 -0800

    revert: Remove Remotion video features
    - Removed /tutorials page with service explainer videos
    - Removed /promo page
    - Removed all Remotion components (FadeLabIntro, FadeLabPromo, ServiceTutorials)
    - Removed IntroSequence component
    - Uninstalled Remotion packages
    
    Site reverted to previous state with Framer Motion micro-interactions only

--- frontend/src/components/remotion/index.ts
+++
@@ -1,3 +0,0 @@
-// Remotion Video Components for FadeLab
-export { FadeLabPromoVideo, fadeLabVideoConfig } from "./FadeLabPromo";
-export { PromoVideoPlayer, PromoVideoCompact, PromoVideoHero } from "./PromoVideoPlayer";
